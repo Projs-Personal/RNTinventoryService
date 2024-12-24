7904046a92da516f6611d591562674004fa15f74
Abhishek <aksharma.27mjm@gmail.com>
2024-12-25 00:12:05 +0530

    inventory final

--- src/main/java/com/inventoryService/inventoryService/controller/InventoryContoller.java
+++ src/main/java/com/inventoryService/inventoryService/controller/InventoryContoller.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +16,7 @@ public class InventoryContoller {
     private final InventoryService inventoryService;
 
     @GetMapping
-    public ResponseEntity<Boolean> isInStock(String skuCode, Integer quantity) {
+    public ResponseEntity<Boolean> isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
         Boolean response = inventoryService.isInStock(skuCode, quantity);
         if (!response)
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
